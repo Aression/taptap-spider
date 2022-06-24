1111fd60692b42e5dea5faad30a159fe2b36af14
Aression <lvdifine@gmail.com>
2022-06-24 10:27:24 +0800

    完成详情和评论爬取部分

--- taptap/items.py
+++ taptap/items.py
@@ -12,16 +12,32 @@ class RankItem(scrapy.Item):
     stat = scrapy.Field()
 
 
-class CategoryItem(scrapy.Item):
-    name = scrapy.Field()
-    id = scrapy.Field()
-    img_url = scrapy.Field()
+# class CategoryItem(scrapy.Item):
+#     name = scrapy.Field()
+#     id = scrapy.Field()
+#     img_url = scrapy.Field()
 
-    price = scrapy.Field
-    stat = scrapy.Field()
-    tags = scrapy.Field()
-    downloads = scrapy.Field()
+#     price = scrapy.Field()
+#     stat = scrapy.Field()
+#     tags = scrapy.Field()
 
 class GameDetailItem(scrapy.Item):
-    name = scrapy.Field()
-    # todo: check elements provided by the api and define proper fields
+    """
+    Game Detail Item, used to store game detail info
+    - related api: app_details
+    """
+    id = scrapy.Field()  # db['data']['id']
+    name = scrapy.Field()  # db['data']['title']
+    tags = scrapy.Field()  # initlized from the spider
+
+    # note that this is a list of following structure:
+    # {'type': 'db['data']['developers']['type']', 'name': db['data']['developers']['name']}
+    companies = scrapy.Field()
+
+    original_price = scrapy.Field()  # db['data']['price']['taptap_original']
+    current_price = scrapy.Field()  # db['data']['price']['taptap_current']
+
+    downloads = scrapy.Field()  # db['data']['stat']['hits_total']
+    vote_info = scrapy.Field()  # db['data']['stat']['vote_info']
+
+    comment = scrapy.Field()  # db['data']['list'][i]['moment']['extended_entities']['reviews'][0]['contents']['text']
